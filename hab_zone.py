4290489a0bb5edf1403744c68b8dc7a633b35a07
clairejrogers <clairjr1@dhcp-172-31-196-067.mobile.uci.edu>
2025-08-05 15:43:02 -0700

    Added comments for the arguments taken by each function

--- hab_zone.py
+++ hab_zone.py
@@ -4,10 +4,23 @@ import astropy
 import habitex
 
 class HabZoneEvaluator:
+    
     def __init__(self):
         pass
 
     def conservative_habzone(self, hostname=None, t_eff=None, dec=None, period=None, mandr=None):
+        """
+        Evaluation of whether a given planet is in the conservative habitable zone,
+        as given by the runaway greenhouse and maximum greenhouse limits in Kopparapu et al. 2013
+
+        Args (all optional):
+            hostname (tuple): Names of the host stars you are interested in (as strings)
+            t_eff (tuple): Tuple of minimum and maximum stellar effective temperatures you are interested in
+            dec (tuple): Tuple of minimum and maximum declinations you are interested in
+            period (tuple): Tuple of minimum and maximum planet orbital periods you are interested in (in days)
+            mandr (Boolean, default=False): Require that there be measured mass and radius values
+
+         """
         #Inner radius = runaway greenhouse (per Kopparapu et al. 2013)
         #Outer radius = maximum greenhouse
         sol_flux_in = 1.0512
@@ -46,6 +59,18 @@ class HabZoneEvaluator:
         return data
 
     def optimistic_habzone(self, hostname=None, t_eff=None, dec=None, period=None, mandr=None):
+        """
+        Evaluation of whether a given planet is in the optimistic habitable zone,
+        as given by the recent Venus and early Mars limits in Kopparapu et al. 2013
+
+        Args (all optional):
+            hostname (tuple): Names of the host stars you are interested in (as strings)
+            t_eff (tuple): Tuple of minimum and maximum stellar effective temperatures you are interested in
+            dec (tuple): Tuple of minimum and maximum declinations you are interested in
+            period (tuple): Tuple of minimum and maximum planet orbital periods you are interested in (in days)
+            mandr (Boolean, default=False): Require that there be measured mass and radius values
+
+        """
         #Inner radius = recent Venus (per Kopparapu et al. 2013)
         #Outer radius = early Mars
         sol_flux_in = 1.7753
